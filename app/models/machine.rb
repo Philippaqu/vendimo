7b19844ee3c021dcacccaa5ba5bc13a30e544427
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-21 11:42:14 +0000

    Adding attachinary to machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -1,4 +1,6 @@
 class Machine < ApplicationRecord
   has_many :categories, through: :machine_categories
   validates :address, presence: true
+  has_attachment :building_photo
+  has_attachment :machine_photo
 end

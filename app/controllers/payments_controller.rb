73e281418ff551c38c454501111d1a0335fd31cb
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-24 00:36:45 +0000

    styling

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,24 +3,26 @@ class PaymentsController < ApplicationController
     @user = current_user
   end
 
-def create
-  customer = Stripe::Customer.create(
-    source: params[:stripeToken],
-    email:  params[:stripeEmail]
-  )
+  def create
+    customer = Stripe::Customer.create(
+      source: params[:stripeToken],
+      email:  params[:stripeEmail]
+      )
 
-  charge = Stripe::Charge.create(
+    charge = Stripe::Charge.create(
     customer:     customer.id,   # You should store this customer id and re-use it.
     amount:       params[:value].to_i,
     description:  "Recharge balance",
     currency:     current_user.balance.currency
-  )
+    )
 
-  current_user.balance_cents += params[:value].to_i
-  current_user.save
-  redirect_to balance_path
 
- #Update user balance
+
+#Update user balance
+current_user.balance_cents += params[:value].to_i
+current_user.save
+redirect_to balance_path
+
 
 rescue Stripe::CardError => e
   flash[:alert] = e.message

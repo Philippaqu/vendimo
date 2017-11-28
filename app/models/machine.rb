3e0c94f79f778299ba8751571fb875880233a998
Philippa Quadt <philippa.quadt@gmail.com>
2017-11-28 15:07:43 +0000

    Generating QR Codes for every machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -5,7 +5,29 @@ class Machine < ApplicationRecord
   validates :address, presence: true
   has_attachment :building_photo
   has_attachment :machine_photo
+  has_attachment :qr_code
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  after_create :create_qrcode
+
+
+
+  def create_qrcode
+    text = self.id.to_s
+    code = RQRCode::QRCode.new(text, size: 3)
+    file = Dir.tmpdir + "/qrcode-#{SecureRandom.hex}.png"
+    png = code.as_png(
+     resize_gte_to: false,
+     resize_exactly_to: false,
+     fill: 'white',
+     color: 'black' ,
+     size: 120,
+     border_modules: 4,
+     module_px_size: 6,
+     file: file # path to write
+    )
+    self.qr_code = File.open(file, 'r')
+end
+
 end
